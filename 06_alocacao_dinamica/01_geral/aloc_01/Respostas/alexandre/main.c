b221d25be99ccfe9098f4576c6b27eb7a7e2f6bc
fardin <alexandrefardinfs@gmail.com>
2025-01-30 10:12:29 -0300

    aloc 1

---
+++ 06_alocacao_dinamica/01_geral/aloc_01/Respostas/alexandre/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main(){
+    int tam;
+    scanf("%d\n",&tam);
+    int vetor[tam];
+    *vetor=CriaVetor(tam);
+    LeVetor(vetor,tam);
+    printf("%.2f\n",CalculaMedia(vetor,tam));
+    LiberaVetor(vetor);
+
+    return 0;
+}
